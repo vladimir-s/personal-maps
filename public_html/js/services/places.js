308aa91c0c63bc190a601f7b2ef1ecf36dd59bbc
Vladimir Statsenko <vovchiksta@gmail.com>
2013-07-25 11:45:23 +0300

    CRUD methods with tests have been added to Places service

--- public_html/js/services/places.js
+++ public_html/js/services/places.js
@@ -6,18 +6,16 @@ angular.module('personalmaps.services', [])
         var places = [];
 
         function getPlaces() {
-            $http({method: 'GET', url: 'api/places'}).
-                success(function(data, status, headers, config) {
+            $http({method: 'GET', url: 'api/places'})
+                .success(function(data, status, headers, config) {
                     places = data;
                     $rootScope.$broadcast('places:updated');
-                }).
-                error(function(data, status, headers, config) {
+                })
+                .error(function(data, status, headers, config) {
                     console.log(data);
                 });
         }
         getPlaces();
-
-
 
         var service = {};
 
@@ -26,7 +24,7 @@ angular.module('personalmaps.services', [])
         }
 
         service.get = function(id) {
-            var place;
+            var place = null;
             angular.forEach(places, function(value) {
                 if (parseInt(value.id) === parseInt(id)) {
                     place = value;
@@ -34,6 +32,43 @@ angular.module('personalmaps.services', [])
                 }
             });
             return place;
+        }
+
+        service.add = function(place) {
+            $http({method: 'POST', url: 'api/places', data: place})
+                .success(function(data, status, headers, config) {
+                    places.push(place);
+                    $rootScope.$broadcast('places:added');
+                })
+                .error(function(data, status, headers, config) {
+                    console.log(data);
+                });
+        }
+
+        service.update = function(place) {
+            $http({method: 'PUT', url: 'api/places/' + place.id})
+                .success(function(data, status, headers, config) {
+                    $rootScope.$broadcast('places:updated');
+                })
+                .error(function(data, status, headers, config) {
+                    console.log(data);
+                });
+        }
+
+        service.delete = function(place) {
+            $http({method: 'DELETE', url: 'api/places/' + place.id})
+                .success(function(data, status, headers, config) {
+                    angular.forEach(places, function(value, i) {
+                        if (parseInt(value.id) === parseInt(place.id)) {
+                            places.splice(i, 1);
+                            return false;
+                        }
+                    });
+                    $rootScope.$broadcast('places:deleted');
+                })
+                .error(function(data, status, headers, config) {
+                    console.log(data);
+                });
         }
 
         return service;
--- public_html/protected/tests/unit/js/services/places.spec.js
+++ public_html/protected/tests/unit/js/services/places.spec.js
@@ -5,13 +5,41 @@ describe('Places service', function() {
 
     var $httpBackend, injector;
 
+    var response = [
+        {
+            'id': '1',
+            'p_title': 'title 1',
+            'p_description': 'desc 1',
+            'p_coords': '0, 0',
+            'p_user': 1
+        },
+        {
+            'id': '2',
+            'p_title': 'title 2',
+            'p_description': 'desc 2',
+            'p_coords': '0, 0',
+            'p_user': 2
+        }
+    ];
+
+    var newPlace = {
+        'id': '3',
+        'p_title': 'title 3',
+        'p_description': 'desc 3',
+        'p_coords': '0, 0',
+        'p_user': 1
+    };
+
     beforeEach(function() {
         module('personalmaps.services');
 
         inject(function($injector) {
             injector = $injector;
             $httpBackend = $injector.get('$httpBackend');
-            $httpBackend.when('GET', 'api/places').respond([]);
+            $httpBackend.when('GET', 'api/places').respond(response);
+            $httpBackend.when('POST', 'api/places').respond(newPlace);
+            $httpBackend.when('PUT', 'api/places/2').respond(response[1]);
+            $httpBackend.when('DELETE', 'api/places/2').respond('');
         });
     });
 
@@ -24,5 +52,63 @@ describe('Places service', function() {
         $httpBackend.expectGET('api/places');
         injector.get('Places');
         $httpBackend.flush();
+    });
+
+    it('returns false for unknown places', function() {
+        var places = injector.get('Places');
+        $httpBackend.flush();
+        expect(places.get('43534535')).toBe(null);
+    });
+
+    it('returns particular place', function() {
+        var places = injector.get('Places');
+        $httpBackend.flush();
+        expect(places.get('1')).toEqual(response[0]);
+    });
+
+    it('returns all places', function() {
+        var places = injector.get('Places');
+        $httpBackend.flush();
+        expect(places.getAll()).toEqual(response);
+    });
+
+    it('should update places from controller', function() {
+        var places = injector.get('Places');
+        $httpBackend.flush();
+        var allPlaces = places.getAll();
+        var firstPlace = places.get('1');
+        firstPlace.p_title = 'test';
+        expect(firstPlace).toEqual(allPlaces[0]);
+    });
+
+    it('should add new places', function() {
+        var places = injector.get('Places');
+        $httpBackend.flush();
+        var allPlaces = places.getAll();
+        $httpBackend.expectPOST('api/places');
+        places.add(newPlace);
+        $httpBackend.flush();
+        expect(places.getAll().length).toEqual(3);
+    });
+
+    it('should update places', function() {
+        var places = injector.get('Places');
+        $httpBackend.flush();
+        var secondPlace = places.get(2);
+        secondPlace.p_title = 'test';
+        $httpBackend.expectPUT('api/places/2');
+        places.update(secondPlace);
+        $httpBackend.flush();
+        expect(places.get(2)).toEqual(secondPlace);
+    });
+
+    it('should delete places', function() {
+        var places = injector.get('Places');
+        $httpBackend.flush();
+        var secondPlace = places.get(2);
+        $httpBackend.expectDELETE('api/places/2');
+        places.delete(secondPlace);
+        $httpBackend.flush();
+        expect(places.get(2)).toEqual(null);
     });
 });
